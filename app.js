f7773adb559582e6d02641cbfce3aa247d138ef6
Albert Ejuku <50700373+albertejuku@users.noreply.github.com>
2020-12-02 11:16:47 +0300

    changes

--- app.js
+++ app.js
@@ -24,7 +24,14 @@ open.addEventListener('click', function(){
         nav.style.boxShadow = '1px 5px 12px black, 0 5px 12px black'
 })
 close.addEventListener('click', function(){
-    nav.style.left = '-75vw'
+    nav.style.left = '-85vw'
     open.style.display = 'block'
     nav.style.boxShadow = ''
 })
+n = document.getElementsByClassName('links')[0];
+for(let i = 0; i < n.children.length; i++){
+    elem = n.children[i];
+    elem.addEventListener('click', function(){
+        alert(this.children[0].innerHTML)
+    })
+}
